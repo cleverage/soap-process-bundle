4315f50836f3495e9338c3e6a58a816dfef147ea
Nicolas Joubert <njoubert@clever-age.com>
2024-10-24 15:17:37 +0200

    #2 Add Makefile & .docker for local standalone usage. Add rector, phpstan & php-cs-fixer configurations

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the CleverAge/SoapProcessBundle package.
+ *
+ * Copyright (c) Clever-Age
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!file_exists(__DIR__.'/src')) {
+    exit(0);
+}
+
+$fileHeaderComment = <<<'EOF'
+    This file is part of the CleverAge/SoapProcessBundle package.
+
+    Copyright (c) Clever-Age
+
+    For the full copyright and license information, please view the LICENSE
+    file that was distributed with this source code.
+    EOF;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PHP71Migration' => true,
+        '@PHP82Migration' => true,
+        '@PHPUnit75Migration:risky' => true,
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'protected_to_private' => false,
+        'native_constant_invocation' => ['strict' => false],
+        'header_comment' => ['header' => $fileHeaderComment],
+        'modernize_strpos' => true,
+        'get_class_to_class_keyword' => true,
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder(
+        (new PhpCsFixer\Finder())
+            ->in(__DIR__.'/src')
+            ->in(__DIR__.'/tests')
+            ->append([__FILE__])
+    )
+    ->setCacheFile('.php-cs-fixer.cache')
+;
